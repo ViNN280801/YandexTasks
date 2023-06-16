fe86b82dae2abd701c5ff0bb140d22fdc8abbbbb
Vladislav <vladislav_semykin01@mail.ru>
2023-06-16 22:43:30 +0300

    Not solved. 5-th test breaks this prog out

--- 187. Программист на пляже/main.cpp
+++ 187. Программист на пляже/main.cpp
@@ -7,22 +7,26 @@ int main()
     short T{}; // Count of tests
     std::cin >> T;
 
-    for (int i{}; i < T; i++)
+    for (int i{}; i < T && (T >= 1 && T <= 1000); i++)
     {
         int n{}; // Count of sunbeds
         std::cin >> n;
 
         std::set<int> sunbeds;
-        for (int j{}; j < n; j++)
+        for (int j{}; j < n && (n >= 1 && n <= 1'000'000); j++)
         {
             int x{};
             std::cin >> x;
+
+            if (x < 0 || x > 1'000'000'000)
+                return EXIT_FAILURE;
+
             sunbeds.insert(x);
         }
 
         int min{std::numeric_limits<int>::max()};
-        for (auto it{std::next(sunbeds.cbegin())}; it != sunbeds.cend(); ++it)
-            min = std::min(min, *it xor *(std::prev(it)));
+        for (auto it{sunbeds.cbegin()}; it != sunbeds.cend(); ++it)
+            min = std::min(min, *it xor *(std::next(it)));
         std::cout << min << '\n';
     }
 
