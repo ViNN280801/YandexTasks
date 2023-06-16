e53830ff05c9be10fce55ed7ca4b0f8908195825
Vladislav <vladislav_semykin01@mail.ru>
2023-06-16 23:31:01 +0300

    Solved

--- 187. Программист на пляже/main.cpp
+++ 187. Программист на пляже/main.cpp
@@ -1,33 +1,45 @@
 #include <iostream>
 #include <limits>
-#include <set>
+#include <algorithm>
 
 int main()
 {
-    short T{}; // Count of tests
+    int T{}; // Count of tests
     std::cin >> T;
 
-    for (int i{}; i < T && (T >= 1 && T <= 1000); i++)
+    // Iterating by tests
+    for (int i = 0; i < T; i++)
     {
         int n{}; // Count of sunbeds
         std::cin >> n;
 
-        std::set<int> sunbeds;
-        for (int j{}; j < n && (n >= 1 && n <= 1'000'000); j++)
-        {
-            int x{};
-            std::cin >> x;
+        // Allocating memory for array
+        int *sunbeds = new int[n];
 
-            if (x < 0 || x > 1'000'000'000)
-                return EXIT_FAILURE;
+        // Filling values of sunbeds
+        for (int j{}; j < n; j++)
+            std::cin >> sunbeds[j];
 
-            sunbeds.insert(x);
-        }
+        std::sort(sunbeds, sunbeds + n);
 
+        // Init minimal value with maximal integer to future comparing
         int min{std::numeric_limits<int>::max()};
-        for (auto it{sunbeds.cbegin()}; it != sunbeds.cend(); ++it)
-            min = std::min(min, *it xor *(std::next(it)));
-        std::cout << min << '\n';
+
+        // Double loop to do XOR each to each sunbed
+        // (except 1st with 1st, 2nd with 2nd, ..., nth with nth) -> because "A XOR A eq 0"
+        for (int j{1}; j < n; j++)
+            // Check local min (XOR between neighbours sunbeds) with global min
+            min = std::min(sunbeds[j] xor sunbeds[j - 1], min);
+
+        // Outputting result
+        std::cout << min << std::endl;
+
+        // Check if pointer do not contain "nullptr" to avoid deallocating it
+        if (sunbeds)
+        {
+            delete[] sunbeds;
+            sunbeds = nullptr;
+        }
     }
 
     return EXIT_SUCCESS;
