a5bd04796db49b92ade6467ae66d579907b33939
Vladislav <vladislav_semykin01@mail.ru>
2023-06-21 15:13:01 +0300

    Code edited. But not checked

--- C++. B. Горе от перфекционизма/main.cpp
+++ C++. B. Горе от перфекционизма/main.cpp
@@ -1,46 +1,47 @@
 #include <iostream>
-#include <algorithm>
 #include <map>
 #include <vector>
+#include <ranges>
+#include <algorithm>
 #include <string>
+#include <sstream>
+#include <cmath>
 
 int main()
 {
-    int N{}, X{}, T{};
+    short N{};
+    int X{};
+    long T{};
     std::cin >> N >> X >> T;
 
     std::vector<int> v(N);
 
-    for (int i{}; i < N; i++)
+    for (short i{}; i < N; i++)
         std::cin >> v.at(i);
 
     std::map<int, int> m;
+    for (short i{1}; i <= N; i++)
+        m[i] = abs(v.at(i - 1) - X);
 
-    for (int i{}; i < N; i++)
-        m[i + 1] = abs(v.at(i) - X);
-
-    std::vector<std::pair<int, int>> m_sorted;
-    for (auto it = m.begin(); it != m.end(); it++)
-        m_sorted.push_back(std::make_pair(it->first, it->second));
-
-    std::sort(m_sorted.begin(), m_sorted.end(), [](std::pair<int, int> a, std::pair<int, int> b)
-              { return a.second < b.second; });
+    std::vector<std::pair<int, int>> m_sorted(m.cbegin(), m.cend());
+    std::ranges::sort(m_sorted, [](std::pair<int, int> a, std::pair<int, int> b)
+                      { return a.second < b.second; });
 
     std::vector<int> sculptures;
-    for (auto it{m_sorted.begin()}; it != m_sorted.end(); ++it)
+    for (auto const &entry : m_sorted)
     {
-        T -= it->second;
+        T -= entry.second;
         if (T >= 0)
-            sculptures.push_back(it->first);
+            sculptures.push_back(entry.first);
         else
             break;
     }
 
     std::cout << sculptures.size() << std::endl;
-    std::string s("");
-    for (size_t i{}; i < sculptures.size(); i++)
-        s += std::to_string(sculptures.at(i)) + " ";
-    std::cout << s << std::endl;
+    std::stringstream ss;
+    for (int const &i : sculptures)
+        ss << i << ' ';
+    std::cout << ss.str() << std::endl;
 
     return EXIT_SUCCESS;
 }
